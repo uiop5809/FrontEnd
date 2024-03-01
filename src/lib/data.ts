b46c8a5c784e0d5bded7a333db878e3ba29c7eae
HIHJH <2170095@ewhain.net>
2024-03-01 13:20:02 +0900

    feat(#6): 광고 아카이브, 트렌드 분석 파트

---
+++ src/lib/data.ts
@@ -0,0 +1,90 @@
+//************************************ 메인 페이지 *************************************/
+export const mainPageData = [
+  {
+    title: "광고 아카이브",
+    description:
+      "아이디어를 얻기 위해 아카이브에서 참고영상을 찾아보세요.\n키워드만 입력하면 AI 객체 인식 기반으로 원하는 광고를 쉽고 빠르게 찾아볼 수 있어요.",
+    card: [
+      {
+        title: "광고 검색",
+        description:
+          "많은 광고들을 한 눈에 모아보고 내가 검색한 광고의 상세정보를 확인할 수 있어요.",
+        img: "/main/AdSearch.png",
+      },
+      {
+        title: "레퍼런스 보드",
+        description:
+          "키워드만 검색하면 내가 원하는 광고 이미지로 나만의 광고 레퍼런스 보드를 생성할 수 있어요.",
+        img: "/main/RefBoard.png",
+      },
+      {
+        title: "광고 데이터 분석",
+        description:
+          "아이작만의 데이터 시각화 기술로 중소 기업의 광고 영향력, AI 데이터맵을 확인할 수 있어요.",
+        img: "/main/DataAnalysis.png",
+      },
+    ],
+  },
+  {
+    title: "트렌드 분석",
+    description:
+      "인물, 키워드 검색을 통해 관련 해시태그와 검색량 추이, 관련 콘텐츠를 확인해보세요.\n키워드 비교 분석으로 공통 연관어와 관련 콘텐츠까지 한번에 데이터를 비교할 수 있어요.",
+    card: [
+      {
+        title: "인물 분석",
+        description:
+          "검색량과 연관어 추이, 브랜드 평판 지수를 통해 인물에 대한 정보를 한번에 확인할 수 있어요.",
+        img: "/main/CharacterAnalysis.png",
+      },
+      {
+        title: "키워드 분석",
+        description:
+          "검색량과 연관어 추이, 네이버 쇼핑 키워드를 통해 정보를 한번에 분석할 수 있어요.",
+        img: "/main/KeywordAnalysis.png",
+      },
+      {
+        title: "비교 분석",
+        description:
+          "인물/키워드 간 비교를 통해 검색량 추이부터 공통 연관어 비교, 콘텐츠까지 확인 할 수 있어요.",
+        img: "/main/Comparative.png",
+      },
+    ],
+  },
+];
+
+export const storyBoardData = [
+  [
+    {
+      img: "/main/story1.png",
+      writer: "유해령",
+      name: "아라리 치킨",
+    },
+    {
+      img: "/main/story2.png",
+      writer: "유해령",
+      name: "나르지오 워킹화",
+    },
+    {
+      img: "/main/story3.png",
+      writer: "유해령",
+      name: "어드래서블 광고",
+    },
+  ],
+  [
+    {
+      img: "/main/story4.png",
+      writer: "유해령",
+      name: "지벤 컴페니웨어 상업광고",
+    },
+    {
+      img: "/main/story5.png",
+      writer: "유해령",
+      name: "싱그라미 마스크 상업광고",
+    },
+    {
+      img: "/main/story6.png",
+      writer: "유해령",
+      name: "디지털 배너 공익광고",
+    },
+  ],
+];
