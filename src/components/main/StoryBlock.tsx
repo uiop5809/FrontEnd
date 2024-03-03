0d438fff4f9a6dabef227716385e3112c79552a8
HIHJH <2170095@ewhain.net>
2024-03-03 23:49:35 +0900

    fix(#6): eslint 오류 수정

--- src/components/main/StoryBlock.tsx
+++ src/components/main/StoryBlock.tsx
@@ -4,70 +4,67 @@ import { storyBoardData } from "@/lib/data";
 import { colors } from "@/styles/theme";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { useState } from "react";
 import styled from "styled-components";
 
 export default function StoryBlock() {
   return (
-    <>
-      <Layout>
-        <Title>스토리보드 제작</Title>
-        <Description>
-          {
-            "텍스트를 입력하면 Ai가 맞춤형 이미지를 생성해주고,\n외부 오픈소스를 넣거나 직접 그림을 그려 스토리보드를 만들 수 있어요."
-          }
-        </Description>
-        <ButtonWrapper>
-          <Button bgColor={colors.main} textColor={colors.white}>
-            새 카피 만들기
-          </Button>
-          <Button bgColor={colors.mainLight6} textColor={colors.main}>
-            이용 가이드
-          </Button>
-        </ButtonWrapper>
-        <CardBlock>
-          {storyBoardData[0].map((v, i) => (
-            <CardRegion>
-              <CardWrapper>
-                <ImageWrapper>
-                  <Image src={v.img} alt={`storyboard-image.${i}`} fill />
-                </ImageWrapper>
-              </CardWrapper>
-              <CardInfoBlock>
-                <CardInfo>
-                  {v.writer} | {v.name}
-                </CardInfo>
-              </CardInfoBlock>
-            </CardRegion>
-          ))}
-        </CardBlock>
-        <CardBlock>
-          {storyBoardData[1].map((v, i) => (
-            <CardRegion>
-              <CardWrapper>
-                <ImageWrapper>
-                  <Image src={v.img} alt={`storyboard-image.${i}`} fill />
-                </ImageWrapper>
-              </CardWrapper>
-              <CardInfoBlock>
-                <CardInfo>
-                  {v.writer} | {v.name}
-                </CardInfo>
-              </CardInfoBlock>
-            </CardRegion>
-          ))}
-        </CardBlock>
-        <LinkButtonWrapper>
-          더 많은 스토리보드 보기
-          <Image
-            src={"/main/arrow-right.svg"}
-            alt="right-arrow"
-            width={18}
-            height={18}
-          />
-        </LinkButtonWrapper>
-      </Layout>
-    </>
+    <Layout>
+      <Title>스토리보드 제작</Title>
+      <Description>
+        {
+          "텍스트를 입력하면 Ai가 맞춤형 이미지를 생성해주고,\n외부 오픈소스를 넣거나 직접 그림을 그려 스토리보드를 만들 수 있어요."
+        }
+      </Description>
+      <ButtonWrapper>
+        <Button bgColor={colors.main} textColor={colors.white}>
+          새 카피 만들기
+        </Button>
+        <Button bgColor={colors.mainLight6} textColor={colors.main}>
+          이용 가이드
+        </Button>
+      </ButtonWrapper>
+      <CardBlock>
+        {storyBoardData[0].map((v, i) => (
+          <CardRegion>
+            <CardWrapper>
+              <ImageWrapper>
+                <Image src={v.img} alt={`storyboard-image.${i}`} fill />
+              </ImageWrapper>
+            </CardWrapper>
+            <CardInfoBlock>
+              <CardInfo>
+                {v.writer} | {v.name}
+              </CardInfo>
+            </CardInfoBlock>
+          </CardRegion>
+        ))}
+      </CardBlock>
+      <CardBlock>
+        {storyBoardData[1].map((v, i) => (
+          <CardRegion>
+            <CardWrapper>
+              <ImageWrapper>
+                <Image src={v.img} alt={`storyboard-image.${i}`} fill />
+              </ImageWrapper>
+            </CardWrapper>
+            <CardInfoBlock>
+              <CardInfo>
+                {v.writer} | {v.name}
+              </CardInfo>
+            </CardInfoBlock>
+          </CardRegion>
+        ))}
+      </CardBlock>
+      <LinkButtonWrapper>
+        더 많은 스토리보드 보기
+        <Image
+          src="/main/arrow-right.svg"
+          alt="right-arrow"
+          width={18}
+          height={18}
+        />
+      </LinkButtonWrapper>
+    </Layout>
   );
 }
 const Layout = styled.div`
