e178a98d5aeff9e60e84d57526df7a4869598833
parkyejin <uiop5809@naver.com>
2024-03-08 06:14:41 +0900

    feat(#25): 아이템 분석 페이지

---
+++ src/app/trend/item/page.tsx
@@ -0,0 +1,12 @@
+import NOSSR from "@/components/common/NOSSR";
+import ItemPage from "@/components/trend/ItemPage";
+
+const Item = async () => {
+  return (
+    <NOSSR>
+      <ItemPage />
+    </NOSSR>
+  );
+};
+
+export default Item;
