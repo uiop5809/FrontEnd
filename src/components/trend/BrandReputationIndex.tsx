81235a2c32f6cf1781530758ed5ab0081b116a1a
parkyejin <uiop5809@naver.com>
2024-03-08 04:22:25 +0900

    feat(#11): 브랜드 평판 지수

--- src/components/trend/BrandReputationIndex.tsx
+++ src/components/trend/BrandReputationIndex.tsx
@@ -1,44 +1,83 @@
 import { styled } from "styled-components";
-import Ranking from "./Ranking";
-import Container from "../common/Container";
-import { brandRequtation } from "@/lib/trend/trendData";
+import Image from "next/image";
+
+const rankData = ["이병헌", "조승우", "김수현"];
 
 const BrandReputationIndex = () => {
   return (
-    <RankingBox>
-      {brandRequtation.map((brand) => (
-        <Ranking
-          key={brand.id}
-          title={brand.title}
-          ranking={brand.ranking}
-          trend={brand.trend}
-          updown={brand.updown}
-        />
-      ))}
-    </RankingBox>
+    <>
+      <Label>
+        영화배우 부문 <span>4위</span>
+      </Label>
+      <Layout>
+        <Genre>영화배우 부문</Genre>
+        <LineBox>
+          <Image src="/trend/line1.svg" alt="line" width={50} height={30} />
+          <Image src="/trend/line2.svg" alt="line" width={50} height={30} />
+          <Image src="/trend/line3.svg" alt="line" width={50} height={30} />
+        </LineBox>
+        <RankBox>
+          {rankData.map((name, index) => (
+            <Rank>
+              <span>{index + 1}위</span> {name}
+            </Rank>
+          ))}
+        </RankBox>
+      </Layout>
+    </>
   );
 };
 
 export default BrandReputationIndex;
 
-const BrandText = styled.div`
-  font-size: 1.2rem;
-  font-weight: 700;
-  margin-bottom: 2rem;
+const Layout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 3rem;
 `;
 
-const RankingBox = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 1rem;
-
-  & > * {
-    flex-basis: calc(50% - 0.5rem); // 요소의 초기 크기를 50%로 설정
-    margin-bottom: 1rem; // 각 요소의 아래 여백 설정
+const Label = styled.div`
+  color: ${({ theme }) => theme.colors.grey5};
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding-left: 0.78rem;
+  span {
+    color: ${({ theme }) => theme.colors.main};
   }
+`;
 
-  & > *:first-child {
-    flex-basis: 100%; // 첫 번째 요소의 크기를 100%로 설정
+const Genre = styled.div`
+  color: ${({ theme }) => theme.colors.grey5};
+  font-size: 1.2rem;
+  font-weight: 700;
+  background: ${({ theme }) => theme.colors.main};
+  border-radius: 100px;
+  width: 40%;
+  text-align: center;
+  padding: 0.6rem 0;
+  margin-left: 0.78rem;
+`;
+
+const LineBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const RankBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  margin-right: 2rem;
+`;
+
+const Rank = styled.div`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.white};
+  span {
+    color: ${({ theme }) => theme.colors.mainLight1};
   }
 `;
