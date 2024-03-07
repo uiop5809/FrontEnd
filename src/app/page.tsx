a02c5da3f312a6cf1349be9f0c0f63d62e7e9665
HIHJH <2170095@ewhain.net>
2024-03-08 06:14:06 +0900

    feat(#21): 카피 제작 페이지 푸터 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import NOSSR from "@/components/common/NOSSR";
+import Footer from "@/components/footer/Footer";
 import MainPage from "@/components/main/MainPage";
 
 const Main = async () => {
   return (
     <NOSSR>
       <MainPage />
+      <Footer />
     </NOSSR>
   );
 };
--- src/app/trend/page.tsx
+++ src/app/trend/page.tsx
@@ -1,10 +1,12 @@
 import NOSSR from "@/components/common/NOSSR";
+import Footer from "@/components/footer/Footer";
 import TrendPage from "@/components/trend/TrendPage";
 
 const Trend = async () => {
   return (
     <NOSSR>
       <TrendPage />
+      <Footer />
     </NOSSR>
   );
 };
