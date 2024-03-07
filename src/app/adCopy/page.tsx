1d37929745fce4185fc4ba6f1580882ea45aa3ff
HIHJH <2170095@ewhain.net>
2024-03-07 18:54:49 +0900

    feat(#21): 레이아웃

---
+++ src/app/adCopy/page.tsx
@@ -0,0 +1,12 @@
+import AdCopyPage from "@/components/adCopy/adCopyPage";
+import NOSSR from "@/components/common/NOSSR";
+
+const AdCopy = async () => {
+  return (
+    <NOSSR>
+      <AdCopyPage />
+    </NOSSR>
+  );
+};
+
+export default AdCopy;
---
+++ src/components/adCopy/AdCopyPage.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import styled from "styled-components";
+
+const AdCopyPage = async () => {
+  return <Layout>adcopy</Layout>;
+};
+
+export default AdCopyPage;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5.5rem;
+`;
