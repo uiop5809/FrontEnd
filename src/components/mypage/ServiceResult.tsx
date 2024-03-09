3558879427e49fbb28998f33121b2b3f0829bf30
parkyejin <uiop5809@naver.com>
2024-03-09 21:02:19 +0900

    refactor(#28): Service 컴포넌트 분리

--- src/components/mypage/ServiceResult.tsx
+++ src/components/mypage/ServiceResult.tsx
@@ -2,9 +2,10 @@
 
 import styled from "styled-components";
 import { colors } from "@/styles/theme";
-import Image from "next/image";
 import { useState } from "react";
 import { useWindowSize } from "@/hooks/useWindowSize";
+import { serviceData } from "@/lib/user/userData";
+import Service from "./Service";
 
 const ServiceResult = () => {
   const size = useWindowSize();
@@ -14,72 +15,14 @@ const ServiceResult = () => {
     <BoxWrapper>
       {categoryIndex == 0 ? (
         <BoxContents style={{ height: size.height * 0.65 }}>
-          <CopyWrapper>
-            <TagWrapper>
-              <Tag color={colors.secondary} background={colors.secondaryLight3}>
-                바디 카피
-              </Tag>
-              <Tag color={colors.grey1} background={colors.grey5}>
-                톤 앤 매너
-              </Tag>
-            </TagWrapper>
-            ‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’
-          </CopyWrapper>
-          <CopyWrapper>
-            <TagWrapper>
-              <Tag color={colors.main} background={colors.mainLight6}>
-                바디 카피
-              </Tag>
-              <Tag color={colors.grey1} background={colors.grey5}>
-                톤 앤 매너
-              </Tag>
-            </TagWrapper>
-            ‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’
-          </CopyWrapper>
-          <CopyWrapper>
-            <TagWrapper>
-              <Tag color={colors.main} background={colors.mainLight6}>
-                바디 카피
-              </Tag>
-              <Tag color={colors.grey1} background={colors.grey5}>
-                톤 앤 매너
-              </Tag>
-            </TagWrapper>
-            ‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’
-          </CopyWrapper>
-          <CopyWrapper>
-            <TagWrapper>
-              <Tag color={colors.main} background={colors.mainLight6}>
-                바디 카피
-              </Tag>
-              <Tag color={colors.grey1} background={colors.grey5}>
-                톤 앤 매너
-              </Tag>
-            </TagWrapper>
-            ‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’
-          </CopyWrapper>
-          <CopyWrapper>
-            <TagWrapper>
-              <Tag color={colors.main} background={colors.mainLight6}>
-                바디 카피
-              </Tag>
-              <Tag color={colors.grey1} background={colors.grey5}>
-                톤 앤 매너
-              </Tag>
-            </TagWrapper>
-            ‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’
-          </CopyWrapper>
-          <CopyWrapper>
-            <TagWrapper>
-              <Tag color={colors.main} background={colors.mainLight6}>
-                바디 카피
-              </Tag>
-              <Tag color={colors.grey1} background={colors.grey5}>
-                톤 앤 매너
-              </Tag>
-            </TagWrapper>
-            ‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’
-          </CopyWrapper>
+          {serviceData.map((data) => (
+            <Service
+              key={data.id}
+              button1={data.button1}
+              button2={data.button2}
+              content={data.content}
+            />
+          ))}
         </BoxContents>
       ) : (
         <></>
@@ -105,47 +48,13 @@ const BoxContents = styled.div`
   &::-webkit-scrollbar-track {
     margin: 1rem;
     border-radius: 10px;
-    background-color: ${colors.grey0};
+    background-color: ${colors.secondaryLight3};
   }
   &::-webkit-scrollbar {
     width: 0.375rem;
   }
   &::-webkit-scrollbar-thumb {
     border-radius: 10px;
-    background-color: ${colors.main};
+    background-color: ${colors.secondary};
   }
-`;
-const CopyWrapper = styled.div`
-  display: flex;
-  width: 49%;
-  flex-direction: column;
-  justify-content: space-between;
-  border-radius: 0.875rem;
-  background: ${colors.grey0};
-  padding: 1rem 1rem 2rem 1rem;
-  color: var(--Basic-White, #fff);
-  text-align: center;
-  font-size: 0.9375rem;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 1.375rem;
-  margin-bottom: 2%;
-`;
-const TagWrapper = styled.div`
-  display: inline-flex;
-  gap: 0.5rem;
-  margin-bottom: 2rem;
-`;
-const Tag = styled.div<{ color: string; background: string }>`
-  padding: 0.375rem 0.875rem;
-  justify-content: center;
-  align-items: center;
-  border-radius: 1.375rem;
-  border: 1px solid ${(props) => props.color};
-  background: ${(props) => props.background};
-  color: ${(props) => props.color};
-  font-size: 0.75rem;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
 `;
---
+++ src/lib/user/userData.ts
@@ -0,0 +1,38 @@
+export const serviceData = [
+  {
+    id: 1,
+    button1: "바디 카피",
+    button2: "톤 앤 매너",
+    content: "‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’",
+  },
+  {
+    id: 2,
+    button1: "바디 카피",
+    button2: "톤 앤 매너",
+    content: "‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’",
+  },
+  {
+    id: 3,
+    button1: "바디 카피",
+    button2: "톤 앤 매너",
+    content: "‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’",
+  },
+  {
+    id: 4,
+    button1: "바디 카피",
+    button2: "톤 앤 매너",
+    content: "‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’",
+  },
+  {
+    id: 5,
+    button1: "바디 카피",
+    button2: "톤 앤 매너",
+    content: "‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’",
+  },
+  {
+    id: 6,
+    button1: "바디 카피",
+    button2: "톤 앤 매너",
+    content: "‘내가 사는 지역을 더 살기 좋게, 더 매력 있게’",
+  },
+];
