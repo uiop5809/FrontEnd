e099d111689ac03fbf457f54ce471bf5288a56a9
HIHJH <2170095@ewhain.net>
2024-03-10 00:44:49 +0900

    feat(#37): 푸터 수정

--- src/app/trend/item/page.tsx
+++ src/app/trend/item/page.tsx
@@ -1,10 +1,12 @@
 import NOSSR from "@/components/common/NOSSR";
+import Footer from "@/components/footer/Footer";
 import ItemPage from "@/components/trend/ItemPage";
 
 const Item = async () => {
   return (
     <NOSSR>
       <ItemPage />
+      <Footer />
     </NOSSR>
   );
 };
