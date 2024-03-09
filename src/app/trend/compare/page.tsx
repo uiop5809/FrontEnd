dec001917d5d4dd4e619f195eed991e7cb7b99f9
parkyejin <uiop5809@naver.com>
2024-03-09 23:21:20 +0900

    feat(#36): 비교 분석 페이지 성별 검색 비중

---
+++ src/app/trend/compare/page.tsx
@@ -0,0 +1,12 @@
+import NOSSR from "@/components/common/NOSSR";
+import ComparePage from "@/components/trend/ComparePage";
+
+const Compare = async () => {
+  return (
+    <NOSSR>
+      <ComparePage />
+    </NOSSR>
+  );
+};
+
+export default Compare;
--- src/components/trend/ComparePage.tsx
+++ src/components/trend/ComparePage.tsx
@@ -2,33 +2,31 @@
 
 import styled from "styled-components";
 import { LineChart } from "./LineChart";
-import RadialChart from "./RadialChart";
-import BarChart from "./BarChart";
 import RelatedHashTag from "./RelatedHashTag";
 import TrendSearch from "./TrendSearch";
 import { useState } from "react";
 import TrendTop from "./TrendTop";
 import ToggleButton from "../common/ToggleButton";
-import RelatedRankChange from "./RelatedRankChange";
+import CompareRadialChart from "./CompareRadialChart";
 
-const ItemPage = () => {
-  const [searchName, setSearchName] = useState("");
+const ComparePage = () => {
+  const [searchWord, setSearchWord] = useState("");
 
   return (
     <Layout>
       <TrendSearch
-        title="아이템"
-        description="검색량과 연관어 추이, 네이버쇼핑 키워드를 통해 한 번에 분석할 수 있어요."
-        placeholder="예) 홈 인테리어, 여행, 강아지"
-        src="/trend/itemAnalysis.png"
-        setSearchName={setSearchName}
+        title="비교 분석"
+        description="인물/키워드 간 비교를 통해 검색량 추이부터 공통 연관어 비교, 관련 콘텐츠까지 확인할 수 있어요."
+        placeholder="예) 예금 vs 적금, 안유진 vs 동원참치 vs 조정석"
+        src="/trend/compareAnalysis.png"
+        setSearchName={setSearchWord}
       />
 
-      {searchName !== "" && (
+      {searchWord !== "" && (
         <>
           <TrendTop />
           <SearchText>
-            <span>"{searchName}"</span>에 대한 인물 분석 결과입니다.
+            <span>"{searchWord}"</span>에 대한 인물 분석 결과입니다.
           </SearchText>
 
           <ContentWrapper width="87.5rem">
@@ -40,17 +38,12 @@ const ItemPage = () => {
           </ContentWrapper>
 
           <InlineContent>
-            <ContentWrapper width="28.125rem">
-              <Title marginBottom="1.88rem">성별 검색 비중</Title>
-              <RadialChart />
-            </ContentWrapper>
-            <ContentWrapper width="28.125rem">
-              <Title marginBottom="1.88rem">연령별 검색 비중 TOP 3</Title>
-              <BarChart />
-            </ContentWrapper>
-            <ContentWrapper width="28.125rem">
-              <Title marginBottom="1.88rem">연관어 순위 변화</Title>
-              <RelatedRankChange />
+            <ContentWrapper width="87.5rem">
+              <Title marginBottom="2.5rem">성별 검색 비중</Title>
+              <RadialChartBox>
+                <CompareRadialChart label="홈 인테리어" woman={56} man={44} />
+                <CompareRadialChart label="샴푸" woman={30} man={70} />
+              </RadialChartBox>
             </ContentWrapper>
           </InlineContent>
 
@@ -68,7 +61,7 @@ const ItemPage = () => {
   );
 };
 
-export default ItemPage;
+export default ComparePage;
 
 const Layout = styled.div`
   display: flex;
@@ -128,4 +121,10 @@ const InlineContent = styled.div`
   //   width: 90%;
   //   height: 100%;
   //   margin: 6rem auto 0 auto; // 임시로
+`;
+
+const RadialChartBox = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 8rem;
 `;
